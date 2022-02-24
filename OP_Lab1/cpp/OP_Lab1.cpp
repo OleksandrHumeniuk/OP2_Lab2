ea71c28ddcb54eac655bdf12d668f8b25d924874
OleksandrHumeniuk <89925253+OleksandrHumeniuk@users.noreply.github.com>
2022-02-24 23:15:38 +0200

    Update OP_Lab1.cpp

--- OP_Lab1/cpp/OP_Lab1.cpp
+++ OP_Lab1/cpp/OP_Lab1.cpp
@@ -13,18 +13,3 @@ int main()
 	printFile(inputName, "Input File:");
 	printFile(outputName, "\nOutput File:");
 }
-
-//word1 word1 word1
-//word2 word2
-//word3
-//word4 word4 word4 word
-//word3
-//word3
-//word5
-
-//abc zxc ab
-//abc zxc zx z z
-//asd g gdsfl abcd abcd
-//e a a a
-//zc zxc
-//zbdcas
